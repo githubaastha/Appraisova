831399bb1a53a970031947e42b4dffa3401ea7f3
Aastha <aastha0808jain@gmail.com>
2026-07-16 23:46:19 +0530

    Fix alias resolution to be OS-agnostic

--- frontend/appraisal-frontend/vite.config.ts
+++ frontend/appraisal-frontend/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'url'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [
-      { find: '@', replacement: '/vercel/path0/frontend/appraisal-frontend/src' }
-    ]
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   }
 })
