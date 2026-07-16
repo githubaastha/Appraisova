93f4e8c5d4f252ca5282b6167dd93da6d2b74287
Aastha <aastha0808jain@gmail.com>
2026-07-16 23:08:11 +0530

    vercel

--- frontend/appraisal-frontend/vite.config.ts
+++ frontend/appraisal-frontend/vite.config.ts
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath } from 'url'
-import { resolve } from 'path'
-
-const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": resolve(__dirname, 'src').replace(/\\/g, '/'),
+      "@": "/vercel/path0/frontend/appraisal-frontend/src",
     },
   },
 })
